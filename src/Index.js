8b91f37eca9976d08f3309ebc2e6b826d550896c
Diegogl91 <garciadiego03@gmail.com>
2021-09-10 16:03:10 +0000

    starwarsblog

---
+++ src/Index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import reactDom from 'react-dom';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '@popperjs/core';
+import 'bootstrap/dist/js/bootstrap.bundle';
+
+import Layout from './Layout';
+
+ReactDOm.render(<Layout />,document.querySelector('#root'));
---
+++ src/Layout.js
@@ -0,0 +1,10 @@
+import react from 'react';
+
+
+const Layout = () => {
+    return(
+        <h1> Componentes</h1>
+    )
+}
+
+export default Layout;
