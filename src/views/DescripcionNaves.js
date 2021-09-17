55f437d89e84aac5550dd1ae5b4b173f9f640703
Diegogl91 <garciadiego03@gmail.com>
2021-09-17 21:44:15 +0000

    descriptions styles

--- src/views/DescripcionNaves.js
+++ src/views/DescripcionNaves.js
@@ -1,7 +1,7 @@
 import React, { useContext, useEffect } from 'react';
 import { Context } from '../store/appContext';
 import { useParams } from 'react-router-dom';
-
+import { imgShips } from '../img/index';
 
 
 const DescripcionNaves = () => {
@@ -20,27 +20,30 @@ const DescripcionNaves = () => {
         <div className="container">
             <br />
             <div className="title"></div>
-                <div className="row">
-                    {
-                        starshipsDesc === null ? (
-                            <div className="spinner-grow text-secondary" role="status">
-                                <span className="visually-hidden">Loading...</span>
-                            </div>
-                        ) : (
-                            <div className="container">
-                                <div className="row">
-                                    <div className="col-md-12 ">
-                                    <h2>{starshipsDesc.properties.name}</h2>
-                                    <p>Model: {starshipsDesc.properties.model}</p>
-                                    <p>Starship Class: {starshipsDesc.properties.starship_class}</p>
-                                    <p>Manufacturer: {starshipsDesc.properties.manufacturer}</p>
-                                    <p>Length: {starshipsDesc.properties.length}</p>
-                                    <p>Crew: {starshipsDesc.properties.crew}</p>
-                                    <p>Passengers: {starshipsDesc.properties.passengers}</p>
+            <div className="row">
+                {
+                    starshipsDesc === null ? (
+                        <div className="spinner-grow text-secondary" role="status">
+                            <span className="visually-hidden">Loading...</span>
+                        </div>
+                    ) : (
+                        <div className="container">
+                            <div className="row d-flex justify-content-center">
+                                <div className="card desc col-sm-8 rounded d-flex justify-content-between">
+                                    <div className="card-left">
+                                        <h2>{starshipsDesc.properties.name}</h2>
+                                        <p>Model: {starshipsDesc.properties.model}</p>
+                                        <p>Starship Class: {starshipsDesc.properties.starship_class}</p>
+                                        <p>Manufacturer: {starshipsDesc.properties.manufacturer}</p>
+                                        <p>Length: {starshipsDesc.properties.length}</p>
+                                        <p>Crew: {starshipsDesc.properties.crew}</p>
+                                        <p>Passengers: {starshipsDesc.properties.passengers}</p>
+                                    </div>
+                                    <div className="imagenDescrip" style={{ backgroundImage: `url(${imgShips[uid]})` }}></div>
                                 </div>
                             </div>
                         </div>
-                )
+                    )
                 }
             </div>
         </div >
--- src/views/DescripcionPlanetas.js
+++ src/views/DescripcionPlanetas.js
@@ -1,7 +1,7 @@
 import React, { useContext, useEffect } from 'react';
 import { Context } from '../store/appContext';
 import { useParams } from 'react-router-dom';
-
+import { imgPlanets } from '../img/index';
 
 
 const DescripcionPlanetas = () => {
@@ -20,25 +20,31 @@ const DescripcionPlanetas = () => {
         <div className="container">
             <br />
             <div className="title"></div>
-                <div className="row">
-                    {
-                        planetsDesc === null ? (
-                            <div className="spinner-grow text-secondary" role="status">
-                                <span className="visually-hidden">Loading...</span>
-                            </div>
-                        ) : (
-                            <div className="container">
-                                <div className="row">
-                                    <div className="col-md-12 ">
-                                    <h2>{planetsDesc.properties.name}</h2>
-                                    <p>Population: {planetsDesc.properties.population}</p>
-                                    <p>Climate: {planetsDesc.properties.climate}</p>
-                                    <p>Terrain: {planetsDesc.properties.terrain}</p>
-                                    <p>Description: {planetsDesc.description}</p>
+            <div className="row">
+                {
+                    planetsDesc === null ? (
+                        <div className="spinner-grow text-secondary" role="status">
+                            <span className="visually-hidden">Loading...</span>
+                        </div>
+                    ) : (
+                        <div className="container">
+                            <div className="row d-flex justify-content-center">
+                                <div className="card desc col-sm-8 rounded d-flex justify-content-between ">
+                                    <div className="card-left">
+                                        <h2>{planetsDesc.properties.name}</h2>
+                                        <p>Diameter: {planetsDesc.properties.diameter}</p>
+                                        <p>Gravity: {planetsDesc.properties.gravity}</p>
+                                        <p>Water surface: {planetsDesc.properties.surface_water}</p>
+                                        <p>Population: {planetsDesc.properties.population}</p>
+                                        <p>Climate: {planetsDesc.properties.climate}</p>
+                                        <p>Terrain: {planetsDesc.properties.terrain}</p>
+                                        <p>Description: {planetsDesc.description}</p>
+                                    </div>
+                                    <div className="imagenDescrip" style={{ backgroundImage: `url(${imgPlanets[uid]})` }}></div>
                                 </div>
                             </div>
                         </div>
-                )
+                    )
                 }
             </div>
         </div >
